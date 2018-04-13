feab4727afc2206e9c1ffc4a5e7ca76c4f2b14de
Rokas <rokas.janickas@gmail.com>
2018-04-13 19:18:52 +0300

    liuks

--- ND/php-03.php
+++ ND/php-03.php
@@ -1,12 +1,17 @@
 <?php
 
-$m = [1, 2, 6, 8, 10];
+$m = [
+    'Elementas1' => 1,
+    'Elementas2' => 2,
+    'Elementas3' => 7,
+    'Elementas4' => 5,
+    'Elementas5' => 8
+];
+foreach ($m as $element => $sk) {
+    if ($sk % 2 !=0) { // jei padalinus is 2, liekana yra, tai nelyg
+        $m[$element] = 0;  // pakeiciam elementa i nuli
+    }
+}
+//print_r($m);
 
 var_dump($m);
-
-$lyg = $m[0];
-foreach($m as $sk){
-    if ($sk % 2 == 0) $lyg = 0; // jei neturi liekanos padalinus
-}                           //is 2, tai lyginis ir pakeiciam i 0
-
-var_dump($lyg);
--- ND/php-04.php
+++ ND/php-04.php
@@ -1,12 +1,16 @@
 <?php
 
-$m = [1, 2, 6, 8, 9];
+
 
 $suma = 0;
-$lyg = $m[0];
+$kiekis = 0;
+$m = [1, 2, 6, 8, 9];
 foreach($m as $sk){
-    if ($sk % 2 == 0) $lyg = $sk;
+    if ($sk % 2 == 0) {     //jei nera liekanos dalinant is 2 tai lyg
+        $suma += $sk; // $suma = $suma + $lyg
+        $kiekis++;   // kieki apskaiciuoja
+    }
 }
-$suma += $lyg; // $suma = $suma + $lyg
-echo $suma;
-echo $suma / count($lyg); // aritmetinis vidurkis: suma/elementu
+echo "Suma: " . $suma . '<br>';
+echo "Kiekis: " . $kiekis . '<br>';
+echo "Vidurkis: " . $suma/$kiekis;
