2a39515b7947d9c027f796faa100e31e9c0fb36c
rokas <rokas.janickas@gmail.com>
2018-05-07 18:42:26 +0300

    a

--- ND/php-file-02-2.php
+++ ND/php-file-02-2.php
@@ -2,14 +2,22 @@
 /**
 Tekstinio redaktoriaus pagalba sukurti failą su teleloto skaičiais (5 x 5). Parašyto PHP skriptą, kuris nuskaito failą ir išveda ekrane teleloto lentelę (5 x 5).
  */
-$f = fopen('Failai/failas2.txt', r);
-$s = fread($f, filesize('Failai/failas2.txt'));
+$f = fopen('php-file-02-2.txt', 'r');
+$s = fread($f, filesize('php-file-02-2.txt'));
 fclose($f);
 
-$m = explode('\r\n', $s);
-
-for($i=0; $i<count($m); $i++){
-    $m[$i] = explode
+$m = explode("\r\n", $s);
+for ($i=0; $i<count($m); $i++){
+    $m[$i] = explode(' ', $m[$i]);
 }
-var_dump($m);
-// UZBAIK !!!!!!!!!!!!!
+//var_dump($m);
+
+echo '<table>';
+foreach($m as $row){
+    echo '<tr>';
+    foreach($row as $cell){
+        echo '<td>' . $cell . '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
