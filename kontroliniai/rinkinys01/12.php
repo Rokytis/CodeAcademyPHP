d23da4fba0b7791a874d6ea6b3f805074352b1fe
Rokas <rokas.janickas@gmail.com>
2018-05-21 21:27:22 +0300

    ok

--- kontroliniai/rinkinys01/12.php
+++ kontroliniai/rinkinys01/12.php
@@ -2,3 +2,5 @@
 /**
 Tarkime turime duomenų bazėje lentelę dviraciai su laukais id, gamintojas, modelis, kaina. Parašykite SQL užklausą  kuri pateiktų visus duomenis apie 3 brangiausius dviračius.
  */
+
+//PADARYK
